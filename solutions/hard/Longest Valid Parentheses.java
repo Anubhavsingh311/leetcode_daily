2df9519edbfa1e571b8c33dbf64cee6b4d639e13
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-09-17 04:48:00 +0530

    Add solution for Longest Valid Parentheses

---
+++ solutions/hard/Longest Valid Parentheses.java
@@ -0,0 +1,18 @@
+// Title: Longest Valid Parentheses
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-valid-parentheses/
+
+        
+        }
+            rightCount++;
+        } else {
+            leftCount++;
+        if (s.charAt(i) == '(') {
+    for (int i = 0; i < s.length(); i++) {
+    
+    int maxLength = 0;
+    int rightCount = 0;
+    int leftCount = 0;
+class Solution {
+    public int longestValidParentheses(String s) {
