9cd8e01bc6c1d8f6c1b44169d5bf85b89974d324
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-09-18 19:54:08 +0530

    Add solution for Maximum Number of Non-Overlapping Substrings

---
+++ solutions/hard/Maximum Number of Non-Overlapping Substrings.java
@@ -0,0 +1,20 @@
+// Title: Maximum Number of Non-Overlapping Substrings
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-number-of-non-overlapping-substrings/
+
+            if (count[c] == 0) {
+
+            int c = s.charAt(i) - 'a';
+        for (int i = 0; i < s.length(); i++) {
+
+        List<Integer> order = new ArrayList<>();
+
+        Arrays.fill(last, -1);
+        Arrays.fill(first, -1);
+        int[] last = new int[26];
+
+    public List<String> maxNumOfSubstrings(String s) {
+        int[] count = new int[26];
+        int[] first = new int[26];
+class Solution {
