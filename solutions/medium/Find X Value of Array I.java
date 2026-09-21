6c75147dfc9f033557c0619745cb945f835dbc7e
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-09-21 18:59:58 +0530

    Add solution for Find X Value of Array I

---
+++ solutions/medium/Find X Value of Array I.java
@@ -0,0 +1,18 @@
+// Title: Find X Value of Array I
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-x-value-of-array-i/
+
+                if (prev[r] > 0) {
+            for (int r = 0; r < k; r++) {
+            curr[val]++;
+            int val = nums[i] % k;
+            long[] curr = new long[k];
+        for (int i = 0; i < n; i++) {
+        long[] prev = new long[k];
+        long[] result = new long[k];
+        int n = nums.length;
+    public long[] resultArray(int[] nums, int k) {
+class Solution {
+                    curr[(int)((long)r * val % k)] += prev[r];
+                }
