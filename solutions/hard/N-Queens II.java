ef87db19bf201885c674fd35ad6daa6b03c5beba
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-09-22 14:41:05 +0530

    Add solution for N-Queens II

---
+++ solutions/hard/N-Queens II.java
@@ -0,0 +1,15 @@
+// Title: N-Queens II
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/n-queens-ii/
+
+    }
+        return count;
+        backtrack(0, n, cols, diag1, diag2);
+        boolean[] diag2 = new boolean[2 * n];
+        boolean[] diag1 = new boolean[2 * n];
+        boolean[] cols = new boolean[n];
+    public int totalNQueens(int n) {
+
+    private int count = 0;
+class Solution {
