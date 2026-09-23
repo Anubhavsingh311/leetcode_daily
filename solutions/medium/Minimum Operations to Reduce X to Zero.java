0ecc09ffdd67deb53432a06471c3f3681f9044f3
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-09-23 16:43:05 +0530

    Add solution for Minimum Operations to Reduce X to Zero

--- solutions/medium/Minimum Operations to Reduce X to Zero.java
+++ solutions/medium/Minimum Operations to Reduce X to Zero.java
@@ -3,14 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
 
-        int maxLen = -1;
-        long sum = 0;
-        int left = 0;
-        int n = nums.length;
-        if (target == 0) return nums.length;
-        long target = total - x;
-        for (int num : nums) total += num;
-        if (total < x) return -1;
-        long total = 0;
-    public int minOperations(int[] nums, int x) {
-class Solution {
+c
