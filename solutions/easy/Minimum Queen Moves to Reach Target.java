1c2515be91e8c1724bec1d9706f9858742f9d4b6
Anubhav Singh <156686918+Anubhavsingh311@users.noreply.github.com>
2026-09-26 20:04:05 +0530

    Add solution for Minimum Queen Moves to Reach Target

---
+++ solutions/easy/Minimum Queen Moves to Reach Target.java
@@ -0,0 +1,5 @@
+// Title: Minimum Queen Moves to Reach Target
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/minimum-queen-moves-to-reach-target/
+
